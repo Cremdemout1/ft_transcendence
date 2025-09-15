7d495cefc4c126e247d868007f7a5c24709fd820
Phantasiae <phantasiae.flr@gmail.com>
2025-09-15 12:58:01 +0100

    ready to merge, raycast done but visuals goofy sorta

--- srcs/backend/src/game/pong/pong_logic.ts
+++ srcs/backend/src/game/pong/pong_logic.ts
@@ -4,12 +4,16 @@ type Paddle2D = {
   height: number;
   depth: number;
   speed: number;
+  max_speed: number;
+  overshoot: number;
   distance_from_face: number;
   active: number;
   up: number;
   down: number;
   left: number;
   right: number;
+  vx: number;
+  vy: number;
 };
 
 type Vec3 = {
@@ -24,8 +28,8 @@ export class GameMath {
     y: 0,
     z: 0,
     velocity: { x: 0.2, y: 0.4, z: -0.1 },
-    radius: 3,
-    reset: 0, //flag for when the ball has just been reset (to tell client to reset the trail)
+    radius: 3.25,
+    reset: 1, //flag for when the ball has just been reset (to tell client to reset the trail)
   };
   private tmp_ball = {
     x: 0,
@@ -48,14 +52,25 @@ export class GameMath {
     y: 0,
     height: 20,
     depth: 2,
-    speed: 1,
+    speed: 0.15,
+    max_speed: 3,
+    overshoot: 0.8,
     distance_from_face: 0,
     active: 0,
-	up: 0,
-	down: 0,
-	left: 0,
-	right: 0
+    up: 0,
+    down: 0,
+    left: 0,
+    right: 0,
+    vx: 0,
+    vy: 0,
   }));
+
+  private hitPoint = {
+    x: 0,
+    y: 0,
+    z: 0,
+    dist: 0,
+  };
 
   private scores = {
     player1: 0,
@@ -67,6 +82,8 @@ export class GameMath {
   };
   private gameArea = { width: 100, height: 100, depth: 100 };
 
+  private collision: number = 0;
+
   public update(
     up: number,
     down: number,
@@ -74,14 +91,15 @@ export class GameMath {
     right: number,
     reset: number
   ) {
-	this.paddles[0].active=1;//this should be in the constructor where we decide how many paddles based on how many players
+    this.paddles[0].active = 1; //this should be in the constructor where we decide how many paddles based on how many players
 
-	this.paddles[0].up=up;//all inputs will be attributed to their respective player here. the server needs to know which input came from which player (base on server client id?)
-	this.paddles[0].down=down;
-	this.paddles[0].left=left;
-	this.paddles[0].right=right;
+    this.paddles[0].up = up; //all inputs will be attributed to their respective player here. the server needs to know which input came from which player (base on server client id?)
+    this.paddles[0].down = down;
+    this.paddles[0].left = left;
+    this.paddles[0].right = right;
     if (reset) this.resetBall();
     else {
+      this.collision = 0;
       this.ball.x += this.ball.velocity.x;
       this.ball.y += this.ball.velocity.y;
       this.ball.z += this.ball.velocity.z;
@@ -91,6 +109,38 @@ export class GameMath {
       //console.log(this.ball.velocity)
       //this.checkScoring();
     }
+    if (this.collision || this.ball.reset) this.raycast();
+  }
+
+  private raycast() {
+    // console.log(this.ball.velocity);
+    let v_x = this.ball.velocity.x;
+    let v_y = this.ball.velocity.y;
+    let v_z = this.ball.velocity.z;
+
+    let p_x = this.ball.x;
+    let p_y = this.ball.y;
+    let p_z = this.ball.z;
+    while (
+      p_x < 50 &&
+      p_x > -50 &&
+      p_y < 50 &&
+      p_y > -50 &&
+      p_z < 50 &&
+      p_z > -50
+    ) {
+      p_x += v_x;
+      p_y += v_y;
+      p_z += v_z;
+    }
+    this.hitPoint.x = p_x;
+    this.hitPoint.y = p_y;
+    this.hitPoint.z = p_z;
+    console.log("---------------------------------");
+    console.log("x: " + p_x);
+    console.log("y: " + p_y);
+    console.log("z: " + p_z);
+    console.log("---------------------------------");
   }
 
   private wallCollisions() {
@@ -104,6 +154,7 @@ export class GameMath {
       if (this.ball.x < 0) this.scores.player1++;
       else this.scores.player2++;
       this.resetBall();
+      this.collision = 1;
     }
 
     if (Math.abs(this.ball.y) >= this.gameArea.height / 2 - this.ball.radius) {
@@ -111,6 +162,7 @@ export class GameMath {
       this.ball.y =
         Math.sign(this.ball.y) *
         (this.gameArea.height / 2 - this.ball.radius - 0.01);
+      this.collision = 1;
     }
 
     if (Math.abs(this.ball.z) >= this.gameArea.depth / 2) {
@@ -118,76 +170,76 @@ export class GameMath {
       this.ball.z =
         Math.sign(this.ball.z) *
         (this.gameArea.depth / 2 - this.ball.radius - 0.01);
+      this.collision = 1;
     }
     //console.log("wall collisions done!");
   }
 
   movePaddle(paddle: Paddle2D) {
-    let moveAmount = paddle.speed;
+    let ax = 0;
+    let ay = 0;
 
-    let horizontal = 0;
-    let vertical = 0;
+    if (paddle.left) ax -= paddle.speed;
+    if (paddle.right) ax += paddle.speed;
+    if (paddle.up) ay += paddle.speed;
+    if (paddle.down) ay -= paddle.speed;
 
-    if (paddle.up) vertical++;
-    if (paddle.down) vertical--;
-    if (paddle.left) horizontal--;
-    if (paddle.right) horizontal++;
+    paddle.vx += ax;
+    paddle.vy += ay;
 
-    moveAmount = horizontal < 0 ? -moveAmount : moveAmount;
-    if (horizontal != 0) paddle.x += moveAmount;
-    moveAmount = paddle.speed;
-    moveAmount = vertical < 0 ? -moveAmount : moveAmount;
-    if (vertical != 0) paddle.y += moveAmount;
-
-    if (
-      Math.abs(paddle.x) >
-      this.gameArea.width / 2 - paddle.height / 2
-    ) {
-      if (paddle.x > 0)
-        paddle.x =
-          this.gameArea.width / 2 - paddle.height / 2;
-      else
-        paddle.x = -(
-          this.gameArea.width / 2 -
-          paddle.height / 2
-        );
+    if (!paddle.left && !paddle.right) {
+      paddle.vx *= paddle.overshoot;
     }
-    if (
-      Math.abs(paddle.y) >
-      this.gameArea.width / 2 - paddle.height / 2
-    ) {
-      if (paddle.y > 0)
-        paddle.y =
-          this.gameArea.width / 2 - paddle.height / 2;
-      else
-        paddle.y = -(
-          this.gameArea.width / 2 -
-          paddle.height / 2
-        );
+    if (!paddle.up && !paddle.down) {
+      paddle.vy *= paddle.overshoot;
+    }
+    paddle.vx = Math.max(
+      -paddle.max_speed,
+      Math.min(paddle.vx, paddle.max_speed)
+    );
+    paddle.vy = Math.max(
+      -paddle.max_speed,
+      Math.min(paddle.vy, paddle.max_speed)
+    );
+
+    paddle.x += paddle.vx;
+    paddle.y += paddle.vy;
+
+    if (Math.abs(paddle.x) > this.gameArea.width / 2 - paddle.height / 2) {
+      if (paddle.x > 0) paddle.x = this.gameArea.width / 2 - paddle.height / 2;
+      else paddle.x = -(this.gameArea.width / 2 - paddle.height / 2);
+      paddle.vx = 0;
+    }
+
+    if (Math.abs(paddle.y) > this.gameArea.height / 2 - paddle.height / 2) {
+      if (paddle.y > 0) paddle.y = this.gameArea.height / 2 - paddle.height / 2;
+      else paddle.y = -(this.gameArea.height / 2 - paddle.height / 2);
+      paddle.vy = 0;
     }
   }
 
-  private dot_product(x: number, y: number, z: number) {//GET VECTOR MAGNITUDE
+  private dot_product(x: number, y: number, z: number) {
+    //GET VECTOR MAGNITUDE
     // SQUARE ROOT of dot product of the SAME VECTOR (get magnitude)
     return Math.sqrt(x * x + y * y + z * z);
   }
 
-  private normalize(_x: number, _y: number, _z: number, vel: number) {//NORMALIZE VECTOR
+  private normalize(_x: number, _y: number, _z: number, vel: number) {
+    //NORMALIZE VECTOR
     const res: Vec3 = { x: _x / vel, y: _y / vel, z: _z / vel };
     return res;
   }
 
-  private calculateDirection(paddle : Paddle2D) {
-    console.log(
-      "velocity before: " +
-        this.tmp_ball.velocity.x +
-        " " +
-        this.tmp_ball.velocity.y +
-        " " +
-        this.tmp_ball.velocity.z
-    );
-    const offset =
-      (this.tmp_ball.y - paddle.y) / (paddle.height / 2);
+  private calculateDirection(paddle: Paddle2D) {
+    // console.log(
+    //   "velocity before: " +
+    //     this.tmp_ball.velocity.x +
+    //     " " +
+    //     this.tmp_ball.velocity.y +
+    //     " " +
+    //     this.tmp_ball.velocity.z
+    // );
+    const offset = (this.tmp_ball.y - paddle.y) / (paddle.height / 2);
     const maxAngle = (75 * Math.PI) / 180;
     const deflection = offset * maxAngle;
 
@@ -207,8 +259,7 @@ export class GameMath {
     // this.tmp_ball.velocity.z = base.z * cos - base.y * sin;
     // this.tmp_ball.velocity.y = base.z * sin + base.y * cos;
 
-    const offsetX =
-      (this.tmp_ball.x - paddle.x) / (paddle.height / 2);
+    const offsetX = (this.tmp_ball.x - paddle.x) / (paddle.height / 2);
     const deflectionX = offsetX * maxAngle;
 
     const vel = this.dot_product(
@@ -257,7 +308,12 @@ export class GameMath {
 
   private paddleCollisions(paddle: Paddle2D) {
     //the "default" paddle is on the xy plane, perpendicular to the z axis. Looking from the outside, positive y is up, positive x is right and NEGATIVE z is front (incoming ball has a positive Z velocity)
-    const height = paddle.height + 0.15; //margin
+    // console.log(this.ball.velocity);
+	// console.log("tmp x velo: "+ this.tmp_ball.velocity.x);
+	// console.log("tmp y velo: "+ this.tmp_ball.velocity.y);
+	// console.log("tmp z velo: "+ this.tmp_ball.velocity.z);
+
+	const height = paddle.height + 0.15; //margin
     if (
       this.tmp_ball.z >=
         this.gameArea.width / 2 -
@@ -270,19 +326,16 @@ export class GameMath {
       this.tmp_ball.x > paddle.x - height / 2 &&
       this.tmp_ball.velocity.z > 0
     ) {
-      console.log(this.ball.velocity);
       console.log("tmp x= " + this.tmp_ball.x);
       console.log("tmp y= " + this.tmp_ball.y);
       console.log("tmp z= " + this.tmp_ball.z);
       console.log("paddle x= " + paddle.x);
       console.log("paddle y= " + paddle.y);
       console.log(
-        "rightmost bound (x needs to be less than):" +
-          (paddle.x + height / 2)
+        "rightmost bound (x needs to be less than):" + (paddle.x + height / 2)
       );
       console.log(
-        "leftmost bound (x needs to be greater than):" +
-          (paddle.x - height / 2)
+        "leftmost bound (x needs to be greater than):" + (paddle.x - height / 2)
       );
       //this.tmp_ball.velocity.z *= -1;
       this.calculateDirection(paddle);
@@ -306,26 +359,27 @@ export class GameMath {
     //if paddle=paddle1 flip axes and values like this, etc etc, i can make a proper flipping function later that just copies and then rotates the vector by multiplying the matrixes
     this.paddles.forEach((paddle: Paddle2D) => {
       if (paddle.active) {
-		this.movePaddle(paddle); //the input needs to be assigned to a player id (on the game server?) which will then decide which paddle is moved, do movePaddle for each player
-		//check sign of 1 axis (is the ball even in that side of the arena?)
+        this.movePaddle(paddle); //the input needs to be assigned to a player id (on the game server?) which will then decide which paddle is moved, do movePaddle for each player
+        //check sign of 1 axis (is the ball even in that side of the arena?)
         //flip ball velocity (USE A MODULAR FUNCTION)
-		//do paddle collision function
-		    this.tmp_ball.velocity.x = -this.ball.velocity.z;
-    this.tmp_ball.velocity.y = this.ball.velocity.y;
-    this.tmp_ball.velocity.z = this.ball.velocity.x;
-    this.tmp_ball.x = -this.ball.z;
-    this.tmp_ball.y = this.ball.y;
-    this.tmp_ball.z = this.ball.x;
-    if (this.paddleCollisions(paddle)) {
-      //paddle1
-      this.ball.velocity.x = this.tmp_ball.velocity.z;
-      this.ball.velocity.y = this.tmp_ball.velocity.y;
-      this.ball.velocity.z = -this.tmp_ball.velocity.x;
-      this.ball.x = this.tmp_ball.z;
-      this.ball.y = this.tmp_ball.y;
-      this.ball.z = -this.tmp_ball.x;
-      console.log(this.ball.velocity);
-    }
+        //do paddle collision function
+        this.tmp_ball.velocity.x = this.ball.velocity.z;
+        this.tmp_ball.velocity.y = this.ball.velocity.y;
+        this.tmp_ball.velocity.z = this.ball.velocity.x;
+        this.tmp_ball.x = this.ball.z;
+        this.tmp_ball.y = this.ball.y;
+        this.tmp_ball.z = this.ball.x;
+        if (this.paddleCollisions(paddle)) {
+          //paddle1
+          this.ball.velocity.x = this.tmp_ball.velocity.z;
+          this.ball.velocity.y = this.tmp_ball.velocity.y;
+          this.ball.velocity.z = this.tmp_ball.velocity.x;
+          this.ball.x = this.tmp_ball.z;
+          this.ball.y = this.tmp_ball.y;
+          this.ball.z = this.tmp_ball.x;
+          this.collision = 1;
+          console.log(this.ball.velocity);
+        }
       }
     });
   }
@@ -352,6 +406,8 @@ export class GameMath {
       ball: { ...this.ball },
       scores: { ...this.scores },
       paddles: { ...this.paddles },
+	  hitPoint: { ...this.hitPoint },
+	  hit: this.collision  || this.ball.reset
       //for each player, send an item of the other_paddles array
     };
   }
